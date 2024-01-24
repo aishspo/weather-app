4eff6ff4b9dfeccb520689f8346a3449b7e3d6a9
sophia <aishspo@gmail.com>
2024-01-24 18:05:18 -0300

    58min, início api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Input from './components/Inputs';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons/>
-      <Inputs/>
+      <TopButtons />
+      <Inputs />
+
+      <TimeAndLocation />
+      <TemperatureAndDetails />
+      
+      <Forecast title="hourly forecast" />
+      <Forecast title="hourly forecast" />
+
+
     </div>
   );
 }
