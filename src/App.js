40e201ea24c9bc335535da210ee53dc89702a7a4
Sophia Costa <aishspo@gmail.com>
2024-01-26 01:52:04 -0300

    1h48min

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
-import Input from './components/Inputs';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [ query, setQuery ] = useState({q: 'berlin'})
+  const [ units, setUnitis ] = useState('metric')
+  const [ weather, setWeather ] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({...query, units}).then(
+        (data) => {
+          setWeather(data);
+        });
+    }
+    
+    fetchWeather();
+    }, [ query, units ])
+  
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      
-      <Forecast title="hourly forecast" />
-      <Forecast title="hourly forecast" />
+    {weather && (
+      <div>
 
+        <TimeAndLocation weather={weather}/>
+        <TemperatureAndDetails weather={weather}/>
+        
+        <Forecast title="hourly forecast" />
+        <Forecast title="hourly forecast" />
+
+      </div>
+
+    )}
 
     </div>
   );
